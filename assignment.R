9b986d8ff31d0bf6fb9a2cd09393a47fc25124c0
wouwou4444 <valery.buz@outlook.com>
2018-10-21 21:13:11 +0200

    Create first chart4

--- assignment.R
+++ assignment.R
@@ -93,3 +93,22 @@ ggplot(as.data.frame(df_baltimore),
   geom_line() +
   theme_minimal() + 
   theme(plot.title = element_text(size = 14, color = "red" ))
+
+# Question 4
+# Based on section 5 of https://www.epa.gov/sites/production/files/2015-07/documents/aerr_final_rule.pdf
+# we can assume that EI.sector contain the information on Fuel combustion *** coal
+indices_coal <- grep(pattern = "fuel.*coal", x = SCC$EI.Sector, ignore.case = TRUE)
+scc_coal <- SCC[indices_coal,]
+
+NEI_1999_2008 <- subset(NEI, year %in% c(1999, 2008))
+table(NEI_1999_2008$year)
+names(NEI_1999_2008)
+names(SCC)
+NEI_1999_2008_coal <- merge(NEI_1999_2008, scc_coal, by = "SCC")
+table(NEI_1999_2008_coal$year)
+ggplot(NEI_1999_2008_coal, aes(x=year, y=Emissions/1000)) +
+  theme_minimal() +
+  ylim(0,600) +
+  ylab("Total Emissions(thousands)") +
+  labs(title="Emissions in USA for Combustion Fuel based on coal") +
+  stat_summary(fun.y = sum, geom ='line')
